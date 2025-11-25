00cbd930ee638aa3cb830bd3a1ac6885edd63076
pipecode66 <pipecode66@users.noreply.github.com>
2025-11-25 16:10:41 -0500

    Mark App component as standalone

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,6 +4,7 @@ import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
